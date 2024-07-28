9dee835cca00976dd1cc504b40c4697a31f6057d
Rahul Sanap <rahulsanap@Rahuls-MacBook-Air.local>
2024-07-27 22:28:40 -0400

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:false,
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
     images:{
         domains:['img.freepik.com']
     }
